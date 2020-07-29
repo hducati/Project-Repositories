2f3b3438ffd5f91e86d5b3470328910307721564
Henrique Ducati <henrique.dcti@gmail.com>
2020-07-28 23:02:01 -0300

    added appointments router

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appoinments = appointmentsRepository.find();
+
+  return response.json(appoinments);
+});
+
+appointmentsRouter.post('/', async (request, response) => {
+  try {
+    const { provider, date } = request.body;
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService();
+
+    const appointment = await createAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentsRouter;
